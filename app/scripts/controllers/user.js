17468e09b5e12abfa206f7600b73b397dd41ea19
asunhs <asunhs@gmail.com>
2014-01-19 14:40:18 +0900

    login first

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -2,11 +2,32 @@
 
 angular.module('dutchpayApp').controller('UserCtrl', function ($scope, UserService) {
     $scope.user = UserService;
+    $scope.regMode = false;
     
-    $scope.login = function () {
+    function login() {
         UserService.login({
             id : $scope.id,
             password : $scope.password
         });
+    }
+    
+    function register() {
+        UserService.register({
+            id : $scope.id,
+            nick : $scope.nick,
+            password : $scope.password
+        });
+    }
+    
+    $scope.submit = function () {
+        if ($scope.regMode) {
+            register();
+        } else {
+            login();
+        }
+    };
+    
+    $scope.resgierMode = function () {
+        $scope.regMode = true;
     };
 });
--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -14,6 +14,18 @@ angular.module('dutchpayApp').factory('UserService', function ($http) {
                 console.log(data);
                 alert('error');
             });
+        },
+        register : function (user) {
+            
+            if (!user) {
+                throw new Error("empty user");
+            }
+            
+            $http.post('/api/register', { id : user.id, nick : user.nick, password : user.password })
+            .error(function(xhr, data) {
+                console.log(data);
+                alert('error');
+            });
         }
     };
 });
